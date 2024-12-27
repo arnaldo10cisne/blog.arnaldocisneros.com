eb37c8feddcd889e335a0e5345c46c49f301b497
Arnaldo Cisneros <arnaldo.jose.cisneros.zambrano@gmail.com>
2024-12-27 16:34:42 -0500

    Added styles to PageHeader

--- src/app/ui/pageHeader/PageHeader.tsx
+++ src/app/ui/pageHeader/PageHeader.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import styles from "./PageHeader.module.scss";
 import classNames from "classnames";
+import { removeWhitespaces } from "@/app/lib/utility_functions";
+import { FONT_ABHAYA_LIBRE, FONT_PALANQUIN } from "@/app/lib/fonts";
+import BACKGROUND_IMAGE_MOCK from "@/app/lib/assets/mock-image.webp";
+import Image from "next/image";
 
 interface PageHeaderProps {
   title: string;
@@ -10,8 +14,35 @@ interface PageHeaderProps {
 export const PageHeader = ({ title, description }: PageHeaderProps) => {
   return (
     <header className={classNames(styles.PageHeader)}>
-      <h1 className={classNames(styles.PageHeaderTitle)}>{title}</h1>
-      <p className={classNames(styles.PageHeaderDescription)}>{description}</p>
+      <Image
+        className={styles.Background}
+        src={BACKGROUND_IMAGE_MOCK}
+        alt={`${removeWhitespaces(title)}-background`}
+      />
+
+      <div
+        className={classNames(
+          styles.Foreground,
+          styles[`${removeWhitespaces(title)}-backgroundcolor`],
+        )}
+      >
+        <h1
+          className={classNames(
+            styles.PageHeaderTitle,
+            FONT_PALANQUIN.className,
+          )}
+        >
+          {title}
+        </h1>
+        <p
+          className={classNames(
+            styles.PageHeaderDescription,
+            FONT_ABHAYA_LIBRE.className,
+          )}
+        >
+          {description}
+        </p>
+      </div>
     </header>
   );
 };
